ccd723aa1e9aad900b1829006dda52dcecc9114d
Ludwig <lgerdes@g2crowd.com>
2017-04-03 17:18:56 -0500

    Seed the database with fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(
+  name: "Ludwig Gerdes",
+  email: "gerdesludwig@gmail.com",
+  password_digest: "password"
+)
+
+5.times do
+  User.create!(
+    name:            Faker::Name.name,
+    email:           Faker::Internet.email,
+    password_digest: Faker::Lorem.word
+  )
+end
+users = User.all
+
+# Create Lists
+25.times do
+  List.create!(
+    user:         users.sample,
+    public:       Faker::Boolean.boolean(0.5),
+    title:        Faker::Hipster.word,
+    description:  Faker::Hipster.sentence(3)
+  )
+end
+lists = List.all
+
+# Create Items
+50.times do
+  Item.create!(
+    list:   lists.sample,
+    name:   Faker::Name.name,
+    body:   Faker::Hipster.paragraph(2)
+  )
+end
+items = Item.all
+
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{List.count} lists created"
+puts "#{Item.count} items created"
