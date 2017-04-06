1964a3740fcc04a000e896512625e1ee6ce58e4a
Ludwig <lgerdes@g2crowd.com>
2017-04-05 19:56:07 -0500

    Refactor base controller and associated tests with private authenticated? method

--- spec/api/v1/controllers/base_controller_spec.rb
+++ spec/api/v1/controllers/base_controller_spec.rb
@@ -3,16 +3,15 @@ require 'rails_helper'
 RSpec.describe Api::V1::BaseController, type: :controller do
   let(:user) { create(:user) }
 
-#  context "authorized user" do
-#    before do
-#      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
-#      controller.authenticate_user
-#    end
- #
-#    describe "#authenticate_user" do
-#      it "finds a user by their authentication token" do
-#        expect(assigns(:current_user)).to eq(my_user)
-#      end
-#    end
-#  end
+  context "authenticated user" do
+    before do
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user.name, user.password_digest)
+    end
+
+    describe "#authenticated?" do
+      it "finds a user by their username and password" do
+        expect(controller.send(:authenticated?)).to eq(true)
+      end
+    end
+  end
 end
