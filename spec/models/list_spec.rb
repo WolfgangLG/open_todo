d4741eda85f32fc59e0c7a7c3c4e7bc423df2223
Ludwig <lgerdes@g2crowd.com>
2017-04-10 01:13:00 -0500

    Add and test for validations to raise unprocessable entity errors in case strong parameters are not met

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe List, type: :model do
   let(:user) { create(:user) }
   let(:list) { create(:list, user: user) }
 
+  it { is_expected.to validate_presence_of(:title) }
+
   describe "attributes" do
     it "should have title and description attributes" do
       expect(list).to have_attributes(title: list.title, description: list.description, user: user )
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
 
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:password_digest) }
+  it { is_expected.to validate_presence_of(:email) }
+
   describe "attributes" do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: user.name, email: user.email)
