fb8fe860d3e3f69fa3d50c46629d8aa2e8c8c0af
Ludwig <lgerdes@g2crowd.com>
2017-04-20 19:17:32 -0500

    Add authentication token to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418224034) do
+ActiveRecord::Schema.define(version: 20170420225125) do
 
   create_table "items", force: :cascade do |t|
     t.integer  "list_id"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170418224034) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "auth_token"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,10 @@ RSpec.describe User, type: :model do
       expect(user).to have_attributes(name: user.name, email: user.email)
     end
   end
+
+  describe "#generate_auth_token" do
+     it "creates a token" do
+       expect(user.auth_token).to_not be_nil
+     end
+   end
 end
